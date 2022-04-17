0d708570b8fe7dce66c923e3edc1aea91c6677e9
fjodor <wolf.riepl@statistik-dresden.de>
2022-04-17 22:15:44 +0200

    t-Test_delayed-react.R: Re-calculate only after button is clicked using eventReactive()

--- t-Test_delayed-react.R
+++ t-Test_delayed-react.R
@@ -13,11 +13,12 @@ ui <- fluidPage(
   sidebarLayout(
     
     sidebarPanel(
-     sliderInput(inputId = "N", label = "N in each Group", min = 10, max = 1000, value = 50),
+     sliderInput(inputId = "N", label = "N in each Group", min = 10, max = 200, value = 30),
      sliderInput(inputId = "mean1", label = "Mean in Group 1", min = 10, max = 50, value = 10),
      sliderInput(inputId = "mean2", label = "Mean in Group 2", min = 10, max = 50, value = 11),
      sliderInput(inputId = "sd1", label = "Standard Deviation in Group 1", min = 1, max = 10, value = 5),
-     sliderInput(inputId = "sd2", label = "Standard Deviation in Group 2", min = 1, max = 10, value = 5)
+     sliderInput(inputId = "sd2", label = "Standard Deviation in Group 2", min = 1, max = 10, value = 5),
+     actionButton(inputId = "button", label = "Calculate!")
     ),
     
     mainPanel(
@@ -28,22 +29,26 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
 
-  output$ttest <- renderText({
-
-    set.seed(2022)
-    data <- data.frame(
+  data <- eventReactive(input$button, {
+    set.seed(1975)
+    data.frame(
       x = c(rnorm(n = input$N, mean = input$mean1, sd = input$sd1),
             rnorm(n = input$N, mean = input$mean2, sd = input$sd2)),
       group = c(rep("Group 1", input$N), rep("Group 2",input$N))
-    )
+    ) 
+  })
+
+    output$ttest <- renderText({
+
+    # data <- data()
     
-    ttest <- t.test(x ~ group, data = data)
+    ttest <- t.test(x ~ group, data = data())
     effsize <- tes(ttest$statistic, n.1 = input$N, n.2 = input$N, verbose = FALSE)
     
-    paste("Mean in Group 1:", round(mean(data$x[data$group == "Group 1"]), 2), "<br>",
-          "Mean in Group 2:", round(mean(data$x[data$group == "Group 2"]), 2), "<br><br>",
-          "Standard Deviation in Group 1:", round(sd(data$x[data$group == "Group 1"]), 2), "<br>",
-          "Standard Deviation in Group 2:", round(sd(data$x[data$group == "Group 2"]), 2), "<br><br>",
+    paste("Mean in Group 1:", round(mean(data()$x[data()$group == "Group 1"]), 2), "<br>",
+          "Mean in Group 2:", round(mean(data()$x[data()$group == "Group 2"]), 2), "<br><br>",
+          "Standard Deviation in Group 1:", round(sd(data()$x[data()$group == "Group 1"]), 2), "<br>",
+          "Standard Deviation in Group 2:", round(sd(data()$x[data()$group == "Group 2"]), 2), "<br><br>",
           "<b>The p value of our t-Test is:", format.pval(ttest$p.value, digits = 2), "</p>",
           "The Effect Size - Cohen's d - is:", effsize$d, "</b><br><br>",
           "More detailed interpretation:<br><br>", report(ttest))
