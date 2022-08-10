1eca61cf70162210ecd419e648be06fb049a95f5
fjodor <wolf.riepl@statistik-dresden.de>
2022-08-10 19:58:58 +0200

    updateSelectInput example from shiny docs using observe()

---
+++ _shiny-doc-updateSelectInput.R
@@ -0,0 +1,28 @@
+# Source: https://shiny.rstudio.com/reference/shiny/latest/updateSelectInput.html
+
+ui <- fluidPage(
+  p("The checkbox group controls the select input"),
+  checkboxGroupInput("inCheckboxGroup", "Input checkbox",
+                     c("Item A", "Item B", "Item C")),
+  selectInput("inSelect", "Select input",
+              c("Item A", "Item B", "Item C"))
+)
+
+server <- function(input, output, session) {
+  observe({
+    x <- input$inCheckboxGroup
+    
+    # Can use character(0) to remove all choices
+    if (is.null(x))
+      x <- character(0)
+    
+    # Can also set the label and select items
+    updateSelectInput(session, "inSelect",
+                      label = paste("Select input label", length(x)),
+                      choices = x,
+                      selected = tail(x, 1)
+    )
+  })
+}
+
+shinyApp(ui, server)
